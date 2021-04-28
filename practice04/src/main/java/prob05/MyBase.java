a4b8c9bec16f6035ea13bc7aab7e1cba0061c8e1
mong_head <camara1melon315@gmail.com>
2021-04-28 17:48:41 +0900

    practice4 solved

---
+++ practice04/src/main/java/prob05/MyBase.java
@@ -0,0 +1,28 @@
+package prob05;
+
+public class MyBase extends Base {
+
+	public void afternoon() {
+		System.out.println("오후도 낮과 마찬가지로 일해야 합니다.");
+	}
+	@Override
+	public void day() {
+		System.out.println("낮에는 열심히 일하자!");
+	}
+
+	@Override
+	public void service(String state) {
+		if(state.equals("낮")) {
+			day();
+		} else if(state.equals("오후")) {
+			afternoon();
+		} else {
+			night();	
+		}
+
+	}
+	
+	
+
+	
+}
