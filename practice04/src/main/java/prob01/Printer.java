2b72ed8005b35b58a30b3b60ef628416c2b3842f
mong_head <camara1melon315@gmail.com>
2021-04-30 16:34:33 +0900

    practice04 : 수정 - 가변변수,template method

--- practice04/src/main/java/prob01/Printer.java
+++ practice04/src/main/java/prob01/Printer.java
@@ -1,16 +1,36 @@
 package prob01;
 
 public class Printer {
-	public static void println(int a) {
-		System.out.println(a);
+//	public static void println(int a) {
+//		System.out.println(a);
+//	}
+//	public static void println(boolean a) {
+//		System.out.println(a);
+//	}
+//	public static void println(double a) {
+//		System.out.println(a);
+//	}
+//	public static void println(String a) {
+//		System.out.println(a);
+//	}
+	public <T> void println(T t) {
+		System.out.println(t);
 	}
-	public static void println(boolean a) {
-		System.out.println(a);
+	
+	public <T> void println(T... ts) {
+		for(T t : ts) {
+			System.out.print(t);
+			System.out.print(" ");
+		}
+		System.out.print("\n");
 	}
-	public static void println(double a) {
-		System.out.println(a);
-	}
-	public static void println(String a) {
-		System.out.println(a);
+
+	//가변변수!
+	public int sum(Integer... nums) {
+		Integer sum = 0;
+		for(Integer i : nums) {
+			sum +=  i;
+		}
+		return sum;
 	}
 }
--- practice04/src/main/java/prob01/PrinterTest.java
+++ practice04/src/main/java/prob01/PrinterTest.java
@@ -9,5 +9,13 @@ public class PrinterTest {
 		printer.println( true );
 		printer.println( 5.7 );
 		printer.println( "홍길동" );
+		
+		printer.println(10,true,5.7,"홍길동");
+		
+		// 가변 변수
+		System.out.println(printer.sum(1,2));
+		System.out.println(printer.sum(1,2,3));
+		System.out.println(printer.sum(1,2,3,4,5));
+		
 	}
 }
